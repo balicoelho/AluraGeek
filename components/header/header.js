975a904a32ed597d878b3f5ca6ca576b2fd424c2
Barbara Coelho <balicoelho@gmail.com>
2023-07-03 13:10:15 -0300

    Finalização HTML e CSS

---
+++ components/header/header.js
@@ -0,0 +1,10 @@
+const loginBtn1 = document.querySelector("[data-loginBtn1]");
+const loginBtn2 = document.querySelector("[data-loginBtn2]");
+loginBtn1.addEventListener(
+  "click",
+  () => (window.location.href = "../../pages/login/login.html")
+);
+loginBtn2.addEventListener(
+  "click",
+  () => (window.location.href = "../../pages/login/login.html")
+);
---
+++ pages/product/product.js
@@ -0,0 +1,5 @@
+const addBtn = document.querySelector("[data-addBtn]");
+addBtn.addEventListener("click", () => {
+  console.log("oi");
+  window.location.href = "../add-products/add-product.html";
+});
