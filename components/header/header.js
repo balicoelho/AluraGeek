b0220df655a05e6014d6768fb009e4fb88c9f3ed
Barbara Coelho <balicoelho@gmail.com>
2023-07-03 13:12:24 -0300

    edição url login

--- components/header/header.js
+++ components/header/header.js
@@ -2,9 +2,9 @@ const loginBtn1 = document.querySelector("[data-loginBtn1]");
 const loginBtn2 = document.querySelector("[data-loginBtn2]");
 loginBtn1.addEventListener(
   "click",
-  () => (window.location.href = "../../pages/login/login.html")
+  () => (window.location.href = "./pages/login/login.html")
 );
 loginBtn2.addEventListener(
   "click",
-  () => (window.location.href = "../../pages/login/login.html")
+  () => (window.location.href = "./pages/login/login.html")
 );
